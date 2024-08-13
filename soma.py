a4c95b07bc5846bd189efc061b92ba64752dfed2
Maycon2f <157986759+Maycon2f@users.noreply.github.com>
2024-08-13 20:02:06 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,5 @@
+def soma(a, b):
+    resultado = a + b
+    return print(f"O resultado da soma de {a} + {b} = {resultado}")
+
+soma(0, 1)
---
+++ subtracao.py
@@ -0,0 +1,12 @@
+def subtracao(a, b):
+    if a > b:
+        resultado = a - b
+        print(f"O resultado da subtração de {a} - {b} = {resultado}")
+        return resultado
+    else:
+        resultado = b - a
+        print(f"O resultado da subtração de {a} - {b} = {resultado}")
+    
+
+
+subtracao(9, 10)
